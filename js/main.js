f03a65e4dd6c9d7ad33e3c7f263849ba5d55b9b5
shogo_inamoto <shogo.ca2011+dev@gmail.com>
2018-03-16 10:03:16 +0800

    Create last slide

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const slides = {
   s1: {
     isFirst: true,
-    title: "調べたい言語を選んでください。",
+    title: "学びたいフレームワークの言語を選んでください。",
     choices: [
       { name: "Ruby", next: "s2" },
       { name: "PHP", next: "s3" },
@@ -9,14 +9,30 @@ const slides = {
     ]
   },
   s2: {
-    title: "調べたいフレームワークを選んでください。",
+    title: "学びたいフレームワークを選んでください。",
     choices: [
-      { name: "Ruby on Rails", next: "s1" },
-      { name: "Sinatora", next: "s1" }
+      { name: "Ruby on Rails", next: "s2_1" },
+      { name: "Sinatora", next: "s2_2" }
     ]
   },
+  s2_1: {
+    title: "学びたい教材を選んでください。",
+    choices: [
+      { name: "Ruby on Rails チュートリアル", next: "s2_1_1" },
+      { name: "Progate", next: "s2_1_2" }
+    ]
+  },
+  s2_1_1: {
+    isLast: true,
+    title: "Ruby on Rails チュートリアル：実例を使ってRailsを学ぼう",
+    description: "マイケルハートル氏が書いたRuby on Railsのチュートリアルを翻訳したサイトです。<br>"
+                 + "1章から11章まであり、読み進めていくとRailsの基礎を学ぶことができます。<br>"
+                 + "Hello Worldから始まり、フォロワーの通知の機能まで、ボリュームが多いチュートリアルです。<br>"
+                 + "ただ、翻訳になっているため、初心者だと少し難しい表現があるかもしれません。",
+    link: "https://railstutorial.jp/"
+  },
   s3: {
-    title: "調べたいフレームワークを選んでください。",
+    title: "学びたいフレームワークを選んでください。",
     choices: [
       { name: "CakePHP", next: "s1" },
       { name: "Laravel", next: "s1" },
@@ -24,7 +40,7 @@ const slides = {
     ]
   },
   s4: {
-    title: "調べたいフレームワークを選んでください。",
+    title: "学びたいフレームワークを選んでください。",
     choices: [
       { name: "Django", next: "s1" },
       { name: "Flask", next: "s1" },
